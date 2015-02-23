7c2c62d1b45715e3d4dc9a293e644c6e1439f318
James Wright <12wrigja@gmail.com>
2015-02-23 12:00:25 -0500

    Update to the Seek function to use torques instead of force addition.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,6 +8,7 @@ public class EnemyScript : MonoBehaviour {
     public float health;
     public float bulletDamage;
     public float rateOfFire;
+    public float detectionDistance;
 
     public GameObject target;
 
@@ -22,18 +23,40 @@ public class EnemyScript : MonoBehaviour {
 
     void FixedUpdate()
     {
-        Seek();
+        Debug.DrawLine(this.transform.position, this.transform.position + new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y, 0), Color.green);
+        if (FindPlayer())
+        {
+            Seek();
+        }
+    }
+
+    bool FindPlayer()
+    {
+        float distance = (target.transform.position - transform.position).magnitude;
+        return distance <= detectionDistance;
     }
 
     void Seek()
     {
         Vector3 vel3d = target.transform.position - this.transform.position;
-        Vector2 vel2d = new Vector2(vel3d.x, vel3d.y);
-        vel2d.Normalize();
-        vel2d *= maxSpeed;
-        this.rigidbody2D.velocity = vel2d;
-        float z = Mathf.Atan2((target.transform.position.y - transform.position.y),(target.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
-        transform.eulerAngles = new Vector3(0, 0, z);
+        Vector2 desiredVelocity = new Vector2(vel3d.x, vel3d.y);
+        Vector3 forwardVector = Vector3.Project(desiredVelocity, transform.up);
+        if(forwardVector. > 0){
+            Debug.DrawLine(this.transform.position, this.transform.position + forwardVector, Color.red);
+            this.rigidbody2D.velocity = forwardVector;
+        }
+        Vector3 localPosition = transform.InverseTransformPoint(target.transform.position);
+        float angle = (Mathf.Atan2(localPosition.y, localPosition.x) * Mathf.Rad2Deg +270) % 360;
+        float rotateForce = ((angle > 180) ? (-1 * (angle - 180)) : angle) * rotationalForce / 180;
+        this.rigidbody2D.AddTorque(rotateForce);
     }
 
+    Vector2 truncate(Vector2 vector, float MaximumMagnitude)
+    {
+        if (vector.magnitude > MaximumMagnitude)
+        {
+            return vector.normalized * MaximumMagnitude;
+        }
+        return vector;
+    }
 }
